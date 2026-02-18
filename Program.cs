2637541ce3e91a1038a871f3ec9d5287e3eeac78
unknown <zainabenitan422@gmail.com>
2026-02-18 13:11:50 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+//This program is welcoming you to programming and telling you her name and best food
+Console.WriteLine("Welcome to Programming");
+Console.WriteLine("My name is Henny.");
+Console.WriteLine("My best food is Snacks and Crunches.");
