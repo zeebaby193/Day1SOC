51bf8a215978726c0eff3dabfa9835116788d8ba
zeebaby193 <zainabenitan422@gmail.com>
2026-02-18 13:58:12 +0100

    feat: Add user information

--- Program.cs
+++ Program.cs
@@ -5,3 +5,9 @@
 Console.WriteLine("Welcome to Programming");
 Console.WriteLine("My name is Henny.");
 Console.WriteLine("My best food is Snacks and Crunches.");
+
+//Q. write a program that prints your name, age and state using coorect syntax and comment.
+Console.WriteLine("My name is Opaleke Zainab.");
+Console.WriteLine("I am 12 years old.");
+Console.WriteLine("I am from Osun State.");
+//This program is showcasing the name, age and state of the user.
